93ef79bc55abbdde7359fc13d8d742035c7484af
aleeteo <aleteo946@gmail.com>
2025-03-20 18:07:46 +0100

    improvements on feature extraction

---
+++ functions/features/compute_descriptors.m
@@ -0,0 +1,40 @@
+function [descriptor_row, feature_names] = compute_descriptors(img, mask, label)
+  % Funzione per calcolare i descrittori di un'immagine e restituire una riga di dati
+  % Input:
+  %   - img: immagine in formato matrice
+  %   - mask: maschera binaria associata all'immagine
+  %   - label: etichetta dell'immagine
+  % Output:
+  %   - descriptor_row: riga di una table con label e descrittori
+  %   - feature_names: opzionale, cell array con i nomi dei descrittori
+
+  % Pre-elaborazione immagine (correzione gamma su canale L*)
+  labImg = rgb2lab(im2double(img));
+  gamma = 0.7;
+  labImg(:,:,1) = ((labImg(:,:,1) / 100) .^ gamma) * 100;
+  img_preprocessed = uint8(lab2rgb(labImg) * 255);
+  
+  % Conversione dell'etichetta in formato categoriale
+  label = categorical(label);
+
+  % Estrazione delle feature
+  [shape_features, shape_names] = compute_shape_descriptors(img_preprocessed, mask);
+  [texture_features, texture_names] = compute_texture_descriptors(img_preprocessed, mask);
+  [color_features, color_names] = compute_color_descriptors(img_preprocessed, mask);
+  [edge_features, edge_names] = compute_edge_descriptors(mask);
+  
+  % Creazione della table separando label e feature numeriche
+  label_table = table(label, 'VariableNames', {'Label'});
+  feature_table = array2table([shape_features, texture_features, color_features, edge_features]);
+
+  % Unire le due table in una riga
+  descriptor_row = [label_table, feature_table];
+
+  % Inizializzazione opzionale dei nomi delle feature
+  feature_names = {};
+  if nargout > 1
+    feature_names = [{'Label'}, shape_names, texture_names, color_names, edge_names];
+  end
+end
+
+
---
+++ functions/features/extract_training_data.m
@@ -0,0 +1,44 @@
+function [data, minmax] = extract_training_data(saveFlag)
+  % Funzione per l'estrazione e il salvataggio dei descrittori di forma e texture
+  % Input: save (opzionale) - se true, salva i dati in un file .mat
+  % Output: 
+  %   data - table contenente le features per ogni immagine (labels comprese)
+  %          e nomi delle features
+  
+  arguments
+    saveFlag {mustBeNumericOrLogical} = false
+  end
+
+  % Lettura dei file
+  [images, masks, labels] = readlists_train();
+  nimages = numel(images);
+
+  % Ottenere i nomi delle feature dalla prima immagine
+  img = imread(images{1});
+  mask = imread(masks{1});
+  [~, feature_names] = compute_descriptors(img, mask, labels(1));
+
+  % Creazione di una table vuota con colonne predefinite
+  feature_types = [{'categorical'}, repmat({'double'}, 1, numel(feature_names) - 1)];
+  data = table('Size', [nimages, numel(feature_names)], ...
+               'VariableTypes', feature_types, ...
+               'VariableNames', feature_names);
+
+  % Estrazione delle feature per ogni immagine
+  for i = 1:nimages
+    img = imread(images{i});
+    mask = imread(masks{i});
+    data(i, :) = compute_descriptors(img, mask, labels(i));
+  end
+
+  % Normalizzazione delle feature 
+  if nargout == 2
+    [data, minmax] = normalize_features(data);
+  else
+    data = normalize_features(data);
+  end
+
+  if saveFlag
+    save('data/training_data.mat', 'data');
+  end
+end
