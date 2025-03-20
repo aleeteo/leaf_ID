e5b935661b3f96804c484bd3572c8106f48f638a
aleeteo <aleteo946@gmail.com>
2025-03-20 17:25:46 +0100

    data: arr to table, now minmax array is returned

--- functions/features/compute_all_descriptors.m
+++ functions/features/compute_all_descriptors.m
@@ -1,67 +1,64 @@
-function [data, feature_names] = compute_all_descriptors()
+function [data, minmax] = compute_all_descriptors()
   % Funzione per l'estrazione e il salvataggio dei descrittori di forma e texture
   % Output: 
-  %   data - matrice numerica contenente le features per ogni immagine (labels comprese)
-  %   feature_names - cell array contenente i nomi delle features
-
-  %% operazioni preliminari
-  % - caricamento delle immagini
-  % - estrazione dei nomi delle features
-  % - Preallocazione
+  %   data - table contenente le features per ogni immagine (labels comprese)
+  %          e nomi delle features
 
   % Lettura dei file
   [images, masks, labels] = readlists_train();
   nimages = numel(images);
   
-  % Lettura della prima immagine per determinare il numero di feature
+  % Determinazione dei nomi delle feature
+  % a del numero di esse
   mask = imread(masks{1});
   img = imread(images{1});
-  
-  % calcolo i nomi per calcolare la lunghezza
-  [~ , shape_names] = compute_shape_descriptors(img, mask);
+  [~, shape_names] = compute_shape_descriptors(img, mask);
   [~, texture_names] = compute_texture_descriptors(img, mask);
   [~, color_names] = compute_color_descriptors(img, mask);
   [~, edge_names] = compute_edge_descriptors(mask);
   
   nfeatures = length(shape_names) + length(texture_names) + ...
-              length(color_names)+ length(edge_names);
-  
-  % Preallocazione della matrice data (nimages x (nfeatures + 1))
-  data = zeros(nimages, nfeatures + 1);
+              length(color_names) + length(edge_names);
   
   % Creazione dell'array con i nomi delle features
   feature_names = [{'Label'}, shape_names, texture_names, color_names, edge_names];
-  
-  %% ciclo sulle singole immagini per l'estrazione
+
+  % Creazione di una table vuota con colonne predefinite
+  data = table('Size', [nimages, nfeatures + 1], ...
+               'VariableTypes', [{'categorical'}, repmat({'double'}, 1, nfeatures)], ...
+               'VariableNames', feature_names);
+
+  %% Estrazione delle feature per ogni immagine
   for i = 1:nimages
     % Lettura della maschera e immagine
     mask = imread(masks{i});
     img = imread(images{i});
 
-    % trasformazione gemma per aumentare il contraste nelle ombre (foglia)
-    % e ridurlo nelle alte luci (sfondo)
+    % Pre-elaborazione immagine
     labImg = rgb2lab(im2double(img));
-    gamma = 0.8;
-    labImg(:,:,1) = ((labImg(:,:,1)/100) .^ gamma)*100;
-    img = uint8(lab2rgb(labImg)*255);
+    gamma = 0.7;
+    labImg(:,:,1) = ((labImg(:,:,1) / 100) .^ gamma) * 100;
+    img = uint8(lab2rgb(labImg) * 255);
     
-    % Conversione dell'etichetta in numero
-    if ischar(labels{i}) || isstring(labels{i})
-        data(i,1) = str2double(labels{i});
-    else
-        data(i,1) = labels{i};
-    end
-    
-    % Calcolo dei descrittori di forma e texture
+    % Conversione dell'etichetta in formato categoriale
+    data.Label(i) = categorical(labels(i));
+
+    % Estrazione delle feature
     shape_features = compute_shape_descriptors(img, mask);
     texture_features = compute_texture_descriptors(img, mask);
     color_features = compute_color_descriptors(img, mask);
     edge_features = compute_edge_descriptors(mask);
     
-    % Assegnazione diretta alla matrice dei dati
-    data(i, 2:end) = [shape_features , texture_features, color_features, edge_features];
+    % Assegnazione dei dati alla table
+    data(i, 2:end) = array2table([shape_features, texture_features, color_features, edge_features]);
   end
 
-  data = normalize_features(data);
-end
+  % Normalizzazione delle feature 
+  % controllo sul numero di output
+  if nargout == 2
+    [data, minmax] = normalize_features(data);
+  else
+    data = normalize_features(data);
+  end
 
+end
--- functions/features/normalize_features.m
+++ functions/features/normalize_features.m
@@ -1,17 +1,23 @@
-function data_normalized = normalize_features(data)
+function [data_normalized, minmax] = normalize_features(data)
   % input: 
-  %   - data: matrice numerica (prima colonna = labels, altre colonne = features)
+  %   - data: table contenente le features da normalizzare e labels (prima colonna)
   % output:
   %   - data_normalized: matrice numerica (prima colonna = labels, altre colonne = features normalizzate)
+  %   - minmax: (opzionale) matrice contenenete i valori minimi e massimi di ogni feature
   
   % Estrarre solo le features (escludendo la colonna delle labels)
   features = data(:, 2:end);
   
-  % Normalizzare le features
+  % Normalizzare le features (normalizzazione min-max)
   features_normalized = features;
+
   for i = 1:size(features, 2)
-    features_normalized(:, i) = (features(:, i) - min(features(:, i))) / (max(features(:, i)) - min(features(:, i)));
+    features_normalized(:, i) = (features(:, i) - min(features(:, i))) ./ (max(features(:, i)) - min(features(:, i)));
   end
 
   data_normalized = [data(:,1), features_normalized];
+  % Restituire min e max solo se richiesto
+  if nargout > 1
+    minmax = [min(features); max(features)];
+  end
 end
