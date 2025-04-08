e93ce4f9ea88debdca3c5ef419edfb571c16f131
aleeteo <aleteo946@gmail.com>
2025-04-08 17:36:34 +0200

    normalize_features fixes

--- functions/features/normalize_features.m
+++ functions/features/normalize_features.m
@@ -2,6 +2,7 @@ function [normData, minmax] = normalize_features(data, minmax, hasLabels)
   % input: 
   %   - data: table contenente le features da normalizzare e labels (prima colonna)
   %   - mimax: (opzionale) matrice contenente i valori minimi e massimi di ogni feature
+  %   - hasLabels: (opzionale) booleano che indica se la prima colonna di data contiene le labels
   % output:
   %   - data_normalized: matrice numerica (prima colonna = labels, altre colonne = features normalizzate)
   %   - minmax: (opzionale) matrice contenenete i valori minimi e massimi di ogni feature
@@ -9,31 +10,34 @@ function [normData, minmax] = normalize_features(data, minmax, hasLabels)
   %controlli sugli argomenti
   arguments
     data table
-    minmax (2, :) table = []
+    minmax table = table()
     hasLabels logical = true
   end
-
-  if size(minmax, 2) ~= size(data, 2) - 1 && size(minmax, 2) ~= 0
-    error('Il numero di colonne di minmax deve essere uguale al numero di colonne di data - 1');
-  end
   
+  % Estrazione delle features
   if hasLabels
-    % Estrarre solo le features (escludendo la colonna delle labels)
     features = data(:, 2:end);
   else
     features = data;
   end
-  
-  % Normalizzare le features (normalizzazione min-max)
-  if size(minmax, 2) ~= 0 
+
+  % Se minmax è vuota, calcola i valori
+  if isempty(minmax)
     minmax = [min(features); max(features)];
+  else
+    if height(minmax) ~= 2 || width(minmax) ~= width(features)
+      error('minmax deve avere 2 righe e lo stesso numero di colonne delle feature.');
+    end
   end
+
   features_normalized = (features - minmax(1, :)) ./ (minmax(2, :) - minmax(1, :));
+
+  % eventuale clipping dei valori normalizzati
+  features_normalized = max(min(features_normalized, 1), 0);  % clipping in [0,1]
 
   if hasLabels
       normData = [data(:,1), features_normalized];
   else
       normData = features_normalized;
   end
-
 end
