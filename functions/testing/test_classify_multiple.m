f061ff5db6d45ebff60cd715174ef2b3d94211a9
aleeteo <aleteo946@gmail.com>
2025-04-08 18:15:12 +0200

    primo tent class di foglie multiple (pessimi risultati)

---
+++ functions/testing/test_classify_multiple.m
@@ -0,0 +1,34 @@
+clear
+close all
+
+
+f = fopen('dataset/03_classes/images.list');
+z = textscan(f,'%s');
+fclose(f);
+images = z{:}; 
+
+f = fopen('dataset/03_classes/masks.list');
+m = textscan(f,'%s');
+masks = m{:};
+fclose(f);
+
+f = fopen('dataset/03_classes/labels.list');
+l = textscan(f,'%s');
+labels = l{:};
+fclose(f);
+
+% clear z m l f
+for i = 1:numel(images)
+  img = imread(images{i});
+  mask = imread(masks{i});
+  label = load(labels{i}).labeledImage;
+  
+  pred = classify_multiple(img, mask);
+
+  confmat = confusionmat(label(:), pred(:));
+  figure;
+  subplot(1,2,1), imagesc(pred);
+  subplot(1,2,2), confusionchart(abs(confmat), 'RowSummary', 'row-normalized', 'ColumnSummary', 'column-normalized'), title('Confusion Matrix');
+  % confusionchart(abs(confmat));
+
+end
