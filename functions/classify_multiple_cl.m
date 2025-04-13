181d574276a2ae6b66a306c86ca49cfcef774b3e
aleeteo <aleteo946@gmail.com>
2025-04-13 16:27:49 +0200

    classify_multiple multiple and test for classlearner models

--- functions/classify_multiple_cl.m
+++ functions/classify_multiple_cl.m
@@ -1,29 +1,41 @@
-function pred = classify_multiple_cl(img, mask, C)
+function pred = classify_multiple_cl(img, mask, C, minmax)
+
+  arguments
+    img (:, :, 3) uint8
+    mask (:, :) logical
+    C
+    minmax (:,:) table = []
+  end
+
   [comps, num_labels] = bwlabel(mask);
   pred = zeros(size(mask));
 
-  load('data/minmax.mat', 'minmax');
+  if size(minmax, 1) == 0
+    load('data/minmax_augmented.mat', 'minmax_augmented');
+  else
+    minmax_augmented = minmax;
+  end
 
-  for i = 1:num_labels
+  for i = 10:num_labels
     item_mask = (comps == i);
-    label = classify_object(img, item_mask, minmax, C);
+    label = classify_object(img, item_mask, minmax_augmented, C);
     pred(item_mask) = label;
   end
 end
 
-function label = classify_object(img, item_mask, minmax, model)
+function label = classify_object(img, item_mask, minmax_augmened, model)
   [desc, f_names] = compute_descriptors(img, item_mask);
-  T = build_feature_table(desc, f_names, minmax);
+  T = build_feature_table(desc, f_names, minmax_augmened);
   % label = predict(model, T);
   label = model.predictFcn(T);
 end
 
-function T = build_feature_table(desc, f_names, minmax)
+function T = build_feature_table(desc, f_names, minmax_augmented)
   % Creazione della tabella
   feature_types = [{'categorical'}, repmat({'double'}, 1, numel(desc) - 1)];
   T = table('Size', [1, numel(f_names)], ...
                   'VariableTypes', feature_types, ...
                   'VariableNames', f_names);
   T(1, :) = desc;
-  T = normalize_features(T, minmax);
+  T = normalize_features(T, minmax_augmented);
 end
