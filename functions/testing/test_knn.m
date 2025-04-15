cdb3e9e4df53083c212c8c76025bc8dc3f86fdcd
aleeteo <aleteo946@gmail.com>
2025-04-15 18:59:49 +0200

    hyperparameters optimization and saveFlag

--- functions/testing/test_knn.m
+++ functions/testing/test_knn.m
@@ -1,16 +1,17 @@
-function C = test_knn(training_data, testing_data, feature_number, k)
+function C = test_knn(training_data, testing_data, feature_number, k, saveFlag)
   arguments
     training_data table
     testing_data table
     feature_number double {isinteger} = 25
     k double {isinteger} = 1
+    saveFlag logical = false
   end
 
   [sub_train, sub_test, feat_names] = select_top_features(training_data, testing_data, feature_number);
   save('data/sel_features.mat', 'feat_names');
 
-  C = fitcknn(sub_train, 'Label', 'NumNeighbors', k, 'Standardize', true);
-  % C = fitcknn(sub_train, 'Label', 'NumNeighbors', k);
+  C = fitcknn(sub_train, 'Label', 'NumNeighbors', k, 'optimizeHyperparameters', 'auto');
+
 
   pred = predict(C, sub_test);
 
@@ -18,4 +19,11 @@ function C = test_knn(training_data, testing_data, feature_number, k)
   f1_score = compute_f1_score(sub_test.Label, pred);
   fprintf('F1 Score: %f\n', f1_score);
   confusionchart(abs(confusion_matrix));
+
+  if saveFlag
+    % Save the model to a file
+    modelFileName = 'data/knn_model.mat';
+    save(modelFileName, 'C');
+    fprintf('Model saved to %s\n', modelFileName);
+  end
 end
