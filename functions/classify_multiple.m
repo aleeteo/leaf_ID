f7dee6475237ce3bce2911407a1e56ce3d648f4d
aleeteo <aleteo946@gmail.com>
2025-04-23 14:46:01 +0200

    classify multiple uses recognizer too, other updates to test func

--- functions/classify_multiple.m
+++ functions/classify_multiple.m
@@ -1,21 +1,34 @@
-function pred = classify_multiple(img, mask, C, minmax)
+function pred = classify_multiple(img, mask, classifier, recognizer, minmax)
+%CLASSIFY_MULTIPLE Classifica gli oggetti segmentati in un'immagine
+
   [comps, num_labels] = bwlabel(mask);
   pred = zeros(size(mask));
 
-  predictor_names = C.PredictorNames; % <- 25 nomi di feature
+  class_names = classifier.PredictorNames;
+  rec_names   = recognizer.PredictorNames;
 
   for i = 1:num_labels
     item_mask = comps == i;
-    label = classify_object(img, item_mask, minmax, C, predictor_names);
+    label = classify_object(img, item_mask, classifier, recognizer, class_names, rec_names, minmax);
     pred(item_mask) = label;
   end
 end
+function label = classify_object(img, item_mask, classifier, recognizer, class_names, rec_names, minmax)
+%CLASSIFY_OBJECT Classifica un singolo oggetto usando riconoscitore e classificatore
 
-function label = classify_object(img, item_mask, minmax, model, predictor_names)
   desc = compute_descriptors(img, item_mask);
-  % T = build_feature_table(desc, f_names, predictor_names, minmax);
   desc = normalize_features(desc, minmax);
-  desc = desc(:, predictor_names);
 
-  label = predict(model, desc);
+  % Fase 1: riconoscimento unknown
+  rec_features = desc(:, rec_names);
+  [rec_pred, ~] = predict(recognizer, rec_features);
+
+  if rec_pred == -1
+    label = 11;  % unknown
+    return;
+  end
+
+  % Fase 2: classificazione della foglia
+  class_features = desc(:, class_names);
+  label = predict(classifier, class_features);
 end
