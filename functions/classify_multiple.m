4b1c37e2c3f00673fd8d3790e253222c1856d5b2
aleeteo <aleteo946@gmail.com>
2025-04-23 13:08:52 +0200

    removed table creation since is made in compute_descriptors

--- functions/classify_multiple.m
+++ functions/classify_multiple.m
@@ -12,19 +12,10 @@ function pred = classify_multiple(img, mask, C, minmax)
 end
 
 function label = classify_object(img, item_mask, minmax, model, predictor_names)
-  [desc, f_names] = compute_descriptors(img, item_mask);
-  T = build_feature_table(desc, f_names, predictor_names, minmax);
-  label = predict(model, T);
-end
+  desc = compute_descriptors(img, item_mask);
+  % T = build_feature_table(desc, f_names, predictor_names, minmax);
+  desc = normalize_features(desc, minmax);
+  desc = desc(:, predictor_names);
 
-function T = build_feature_table(desc, f_names, predictor_names, minmax)
-  % Creazione della tabella
-  feature_types = [{'categorical'}, repmat({'double'}, 1, numel(desc) - 1)];
-  T_full = table('Size', [1, numel(f_names)], ...
-                  'VariableTypes', feature_types, ...
-                  'VariableNames', f_names);
-  T_full(1, :) = desc;
-  T_full = normalize_features(T_full, minmax);
-  % Seleziona solo le feature specificate in predictor_names
-  T = T_full(:, predictor_names);
+  label = predict(model, desc);
 end
