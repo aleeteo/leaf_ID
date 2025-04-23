8f33f6e968780b7e7bca7e72f50bf066b72fee14
aleeteo <aleteo946@gmail.com>
2025-04-23 12:58:01 +0200

    texture descriptors computed only on object pixels

--- functions/features/compute_texture_descriptors.m
+++ functions/features/compute_texture_descriptors.m
@@ -9,12 +9,12 @@ function texture_table = compute_texture_descriptors(img, mask)
       img = rgb2gray(img);
   end
 
-  % Applica maschera all'immagine
-  masked_img = img .* uint8(mask);
+  % Selezione dei soli pixel della maschera
+  roi_pixels = double(img(mask));
 
   % Calcolo delle feature
-  [glcm_features, glcm_feature_names] = compute_glcm_features(masked_img);
-  [stat_features, stats_feature_names] = compute_hist_stats_descriptors(masked_img);
+  [glcm_features, glcm_feature_names] = compute_glcm_features(img, mask);
+  [stat_features, stats_feature_names] = compute_hist_stats_descriptors(roi_pixels);
   avg_edge = compute_avg_edge(img, mask);
 
   % Aggregazione
@@ -25,56 +25,48 @@ function texture_table = compute_texture_descriptors(img, mask)
   texture_table = array2table(features, 'VariableNames', feature_names);
 end
 
-function [features, feature_names] = compute_glcm_features(img)
-  % Funzione per il calcolo delle caratteristiche (features) basate sulla matrice di co-occorrenza dei livelli di grigio (GLCM).
-  % Input: 
-  %   - img: immagine in scala di grigi o immagine a colori (che verrà convertita in scala di grigi).
-  % Output: 
-  %   - features: vettore contenente le caratteristiche calcolate dalla GLCM.
-  %   - feature_names: vettore contenente i nomi delle caratteristiche calcolate dalla GLCM.
-
-  % Controllo input
-  if nargin < 1
-    error('Devi fornire un''immagine.');
+function [features, feature_names] = compute_glcm_features(img, mask)
+  % Ritaglia bounding box della regione della maschera
+  props = regionprops(mask, 'BoundingBox');
+  if isempty(props)
+      error('La maschera non contiene regioni valide.');
   end
+  bb = round(props(1).BoundingBox);
+  roi = imcrop(img, bb);
+  roi_mask = imcrop(mask, bb);
+  roi(~roi_mask) = 0;
 
-  % Calcolo della matrice di co-occorrenza (GLCM)
-  offsets = [0 1; -1 1; -1 0; -1 -1]; % Direzioni multiple
-  glcm = graycomatrix(img, ...
+  % Calcolo della GLCM (su ROI)
+  offsets = [0 1; -1 1; -1 0; -1 -1];
+  glcm = graycomatrix(roi, ...
                       'Offset', offsets, ...
                       'Symmetric', true, ...
-                      'NumLevels', 32); % Riduzione livelli di grigio
+                      'NumLevels', 32);
 
-  % estrazione delle feature statistiche relative alla GLCM
   stats = graycoprops(glcm, {'Contrast', 'Correlation', 'Energy', 'Homogeneity'});
-  
-  % Creazione del vettore delle features
+
   features = [mean(stats.Contrast), mean(stats.Correlation), ...
               mean(stats.Energy), mean(stats.Homogeneity)];
-  
-  feature_names = {'Contrast', 'Correlation', 'Energy', 'Homogeneity'};
 
+  feature_names = {'Contrast', 'Correlation', 'Energy', 'Homogeneity'};
 end
 
-function [features ,feature_names] = compute_hist_stats_descriptors(image)
-  % Converte l'immagine in scala di grigi se necessario
-  if size(image, 3) == 3
-      image = rgb2gray(image);
-  end
-  
-  % Calcola l'istogramma e normalizza
-  [counts, levels] = imhist(image);
-  p = counts / sum(counts); % Probabilità dei livelli di intensità
-  
-  % Calcolo delle feature statistiche
-  mean_val = sum(levels .* p); % Media
-  variance = sum(((levels - mean_val) .^ 2) .* p); % Varianza
-  skewness = sum(((levels - mean_val) .^ 3) .* p) / (sqrt(variance)^3); % Asimmetria
-  kurtosis = sum(((levels - mean_val) .^ 4) .* p) / (variance^2) - 3; % Curtosi
-  energy = sum(p .^ 2); % Energia
-  entropy_val = -sum(p(p>0) .* log2(p(p>0))); % Entropia
-  
-  features = [mean_val, variance, skewness, kurtosis, energy, entropy_val];
+function [features ,feature_names] = compute_hist_stats_descriptors(roi_pixels)
+  p = histcounts(roi_pixels, 0:256); % istogramma a 256 livelli
+  p = p / sum(p); % normalizzazione
+
+  levels = 0:255;
+  levels = levels(p > 0); % solo livelli presenti
+  p = p(p > 0);           
+
+  mean_val = sum(levels .* p);
+  variance = sum(((levels - mean_val).^2) .* p);
+  skewness_val = sum(((levels - mean_val).^3) .* p) / (sqrt(variance)^3);
+  kurtosis_val = sum(((levels - mean_val).^4) .* p) / (variance^2) - 3;
+  energy = sum(p.^2);
+  entropy_val = -sum(p .* log2(p));
+
+  features = [mean_val, variance, skewness_val, kurtosis_val, energy, entropy_val];
   feature_names = {'Media', 'Varianza', 'Asimmetria', 'Curtosi', 'Energia', 'Entropia'};
 end
 
