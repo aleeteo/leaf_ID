07b88eb4d357796ad0657e86d286af77e6a9ded9
aleeteo <aleteo946@gmail.com>
2025-05-15 14:55:07 +0200

    0.98 accuracy for both classifier and detector

--- functions/utility/test_system.m
+++ functions/utility/test_system.m
@@ -1,10 +1,10 @@
 %% setting script variables
 load_data = true;
-load_bestIdx = true;
+load_bestIdx = false;
 load_classifier = true;
 load_detector = true;
-classifier_model_type = "svm"; %"svm", "tree", "knn", "bayes", "ensamble", "auto"
-optimize_classifier = true;
+classifier_model_type = "knn"; %"svm", "tree", "knn", "bayes", "ensamble", "auto"
+optimize_classifier = false;
 
 %% data loading/extraction
 if load_data
@@ -24,7 +24,7 @@ if load_bestIdx
   bestIdx = load("data/selected_features.mat", "bestSet").bestSet;
 else
   fprintf("selecting features...\n");
-  bestIdx = select_best_features(training_data, "Label", Verbose=false);
+  bestIdx = select_best_features(training_data, "Label", Verbose=true, NumFeatRange=[23, 27]);
 end
 
 sub_training_data = training_data(:, [1 bestIdx]);
@@ -36,8 +36,8 @@ if load_classifier
   load("data/classifier.mat", "classifier", "test_accuracy");
 else
   fprintf("training classifier...\n");
-  classifier = test_classification(sub_training_data, sub_testing_data, ...
-                                   feature_number=0, saveFlag=false, ...
+  [classifier, test_accuracy] = test_classification(sub_training_data, sub_testing_data, ...
+                                   saveFlag=false, ...
                                    model=classifier_model_type, ...
                                    optimize=optimize_classifier);
 end
@@ -46,11 +46,12 @@ fprintf("classifier accuracy on test_set: %.2f%%\n", test_accuracy * 100);
 %% detector 
 if load_detector
   fprintf("loading detector...\n");
-  detector = load("data/detector.mat", "detector").detector;
+  load("data/detector.mat", "detector", "test_accuracy");
 else
   fprintf("training leaf detector...\n");
-  detector = test_svm_detector(sub_training_data, sub_testing_data, feature_number=0, saveFlag=false);
+  [detector, test_accuracy] = test_svm_detector(sub_training_data, sub_testing_data, feature_number=0, saveFlag=false);
 end
+fprintf("detector accuracy on test_set: %.2f%%\n", test_accuracy * 100);
 
 %% testing on more images
 fprintf("testing on training and test set (instance based)\n")
@@ -73,6 +74,3 @@ test_classify_multiple(classifier, detector, scaling_data, ...
                        standardize=true, parallelize=true, ...
                        visualize_predictions=false, ...
                        visualize_confmat=false);
-
-
-
